ac4e2ce9365007b9fac2f4e7bcdecda5daf180e5
Tyler <tyler@calabrese.nyc>
2024-02-05 20:22:47 -0500

    return an empty JSON list on post to /rides

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+function ridesJSON() {
+    return JSON.stringify([]);
+}
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
+  .post("/rides", (req, res) => res.send(ridesJSON()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
